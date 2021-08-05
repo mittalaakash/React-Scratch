7acf860fcb6ffbf057e014d16972735f64aecc4a
mittalaakash <mit.ash.619@gmail.com>
2021-08-05 13:17:52 +0530

    purecomponent

--- src/Components/ChildComponent.js
+++ src/Components/ChildComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function ChildComponent({ greetParent }) {
+function ChildComponent(props) {
+  console.log(props);
   return (
     <div>
-      <button onClick={() => greetParent('hello')}>Greet Parent</button>
-      <button onClick={greetParent.bind(this, 'hello')}>Greet Parent</button>
+      <button onClick={() => props.greetParent('hello')}>Greet Parent</button>
+      <button onClick={props.greetParent.bind(this, 'hello')}>
+        Greet Parent
+      </button>
     </div>
   );
 }
